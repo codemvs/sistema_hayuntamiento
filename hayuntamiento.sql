eb479a7b7324ca71f233fb0b26fbb4a0165d8be6
codemvs <codemvs@gmail.com>
2020-03-04 22:35:42 -0600

    sql

--- hayuntamiento.sql
+++ hayuntamiento.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-02-2020 a las 05:47:47
+-- Tiempo de generación: 05-03-2020 a las 05:35:05
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.1
 
@@ -73,10 +73,36 @@ CREATE TABLE `tblescritorio` (
 --
 
 INSERT INTO `tblescritorio` (`idEscritorio`, `numeroInventario`, `modelo`, `color`, `areaAdscripcion`, `fechaAdquisicion`, `observacion`, `fotografia`) VALUES
-(2, 454, 'fr54', 'blanco', 'local', '0000-00-00', 'ok', 'foto'),
-(3, 454, 'fr54', 'blanco', 'local', '0000-00-00', 'ok', 'foto'),
-(4, 454, 'fr54 OK', 'blanco', 'local', '0000-00-00', 'ok', 'foto'),
-(5, 454, 'fr54 OK', 'blanco', 'local', '0000-00-00', 'ok ACT', 'foto');
+(3, 454, 'fr54', 'blanco', 'local', '0000-00-00', 'oñalsdñlmsñdlm sajldkjjñl alsjñdljasd', ''),
+(6, 454, 'fr', 'blanco', 'local', '0000-00-00', 'ok', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblotros`
+--
+
+CREATE TABLE `tblotros` (
+  `idOtros` int(11) NOT NULL,
+  `numeroInventario` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `marca` varchar(50) NOT NULL,
+  `precio` double NOT NULL,
+  `condiciones` varchar(100) NOT NULL,
+  `color` varchar(20) NOT NULL,
+  `modelo` varchar(50) NOT NULL,
+  `noPiezas` int(11) NOT NULL,
+  `areaAdscripcion` varchar(50) NOT NULL,
+  `fechaAdquisicion` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `tblotros`
+--
+
+INSERT INTO `tblotros` (`idOtros`, `numeroInventario`, `nombre`, `marca`, `precio`, `condiciones`, `color`, `modelo`, `noPiezas`, `areaAdscripcion`, `fechaAdquisicion`) VALUES
+(4, 907, 'kjb', 'jb', 32, 'kjb', 'kjb', 'kjb', 23, 'kj', '2020-03-18'),
+(5, 97, 'oi', 'oh', 9, 'khh', 'oi', 'oi', 87, 'uiy', '2020-03-17');
 
 -- --------------------------------------------------------
 
@@ -108,7 +134,19 @@ INSERT INTO `tblusuario` (`idUsuario`, `nombre`, `apellidos`, `domicilio`, `tele
 (7, 'carnaval', '2020', 'hl', 'kh', '', '0000-00-00', 'hoi', 'hoi', 'hoi', 'hoi'),
 (8, 'carnaval', '2020', 'hl', 'kh', '565', '0000-00-00', 'hoi', 'hoi', 'hoi', 'hoi'),
 (9, 'prueba', 'p', 'p', 'p', 'p', '0000-00-00', 'p', 'sa', 'popeye@marino.soy', '123'),
-(10, 'IOH', 'OIH', 'IOH', 'OI', 'OIH', '0000-00-00', 'oi', 'ih', 'prueba@prueba.com', '123');
+(10, 'IOH', 'OIH', 'IOH', 'OI', 'OIH', '0000-00-00', 'oi', 'ih', 'prueba@prueba.com', '123'),
+(12, 'moraes', '', '', '', '', '0000-00-00', '', '', 'code@gmail.com', '12'),
+(13, 'as', '', '', '', '', '0000-00-00', '', '', 'as@f.com', 'a'),
+(14, '', '', '', '', '', '0000-00-00', '', '', 'l@g.com', '4234'),
+(15, 'dfsd', '', '', '', '', '0000-00-00', '', '', 'com@gmail.com', '123'),
+(16, '', '', '', '', '', '0000-00-00', '', '', '', ''),
+(17, 'as', '', '', '', '', '0000-00-00', '', '', '', ''),
+(18, '', '', '', '', '', '0000-00-00', '', '', '', ''),
+(19, '', '', '', '', '', '0000-00-00', '', '', '', ''),
+(20, '', '', '', '', '', '0000-00-00', '', '', '', ''),
+(21, '', '', '', '', '', '0000-00-00', '', '', '', ''),
+(22, '', '', '', '', '', '0000-00-00', '', '', '', ''),
+(23, '', '', '', '', '', '0000-00-00', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -141,7 +179,7 @@ CREATE TABLE `tblvehiculo` (
 --
 
 INSERT INTO `tblvehiculo` (`idVehiculo`, `numeroInventario`, `numeroSerie`, `marca`, `mdelo`, `color`, `linea`, `numeroMotor`, `placa`, `areaAdscripcion`, `condicionVehiculo`, `fechaAquisicion`, `observacion`, `descripcion`, `valorActual`, `valorFactura`, `fotografia`) VALUES
-(2, 20, 10, 'marca', '1999', 'blanco', '3', 32343, 'p09ki', 'sala', 'buena', '0000-00-00', '', 'balcon', 1998, 188, 'foto');
+(6, 22, 1212, 'marca', '1999', 'blanco', '3', 32343, 'p09ki', 'sala', 'buena', '0000-00-00', '', 'balcon', 1998, 188, '');
 
 --
 -- Índices para tablas volcadas
@@ -158,6 +196,12 @@ ALTER TABLE `tblcomputadora`
 --
 ALTER TABLE `tblescritorio`
   ADD PRIMARY KEY (`idEscritorio`);
+
+--
+-- Indices de la tabla `tblotros`
+--
+ALTER TABLE `tblotros`
+  ADD PRIMARY KEY (`idOtros`);
 
 --
 -- Indices de la tabla `tblusuario`
@@ -185,19 +229,25 @@ ALTER TABLE `tblcomputadora`
 -- AUTO_INCREMENT de la tabla `tblescritorio`
 --
 ALTER TABLE `tblescritorio`
-  MODIFY `idEscritorio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idEscritorio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `tblotros`
+--
+ALTER TABLE `tblotros`
+  MODIFY `idOtros` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `tblusuario`
 --
 ALTER TABLE `tblusuario`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT de la tabla `tblvehiculo`
 --
 ALTER TABLE `tblvehiculo`
-  MODIFY `idVehiculo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idVehiculo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
